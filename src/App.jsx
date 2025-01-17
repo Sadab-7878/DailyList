9bd22b0b14923fe87e5f8618e8fd4f3132a5135f
Sadab Uz Zaman <57608969+Sadab-7878@users.noreply.github.com>
2025-01-18 03:12:24 +0600

    fully update
    done

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
     < >
     <Navbar/> 
        <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[35%]">
-        <h1 className='font-bold text-center text-3xl'>DailyLife - Manage your todos at one place</h1>
+        <h1 className='font-bold text-center text-3xl'>DailyList - Manage your todos at one place</h1>
          <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className='text-2xl font-bold'>Add a Todo</h2>
           <div className="flex">
